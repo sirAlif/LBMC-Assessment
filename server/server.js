3e27fc7a2a2cb3ef4fbecf705239259df49642b9
Ali Farhadi <ali.farhadi@uniqcast.com>
2024-12-21 14:57:48 +0100

    Implemented the new route for fetching smart contract data and showing it in frontend

--- server/server.js
+++ server/server.js
@@ -24,11 +24,51 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/tokens', require('./routes/api/tokens'));
 app.use('/api/token-pairs', require('./routes/api/tokenPairs'));
 
+// New API route - Ali Farhadi
+app.get('/api/AliFarhadiapitest/:address', async (req, res) => {
+  const { address } = req.params;
+  const apiKey = process.env.ETHERSCAN_API_KEY || "A1TWHBXMKWJ836QBTCFAU1HR94PE7NHGWN";
+
+  if (!/^0x[a-fA-F0-9]{40}$/.test(address)) {
+    return res.status(400).json({ error: 'Invalid Ethereum address' });
+  }
+
+  try {
+    // Fetch details about the given smart contract from Etherscan API
+    const response = await axios.get('https://api.etherscan.io/api', {
+      params: {
+        module: 'contract',
+        action: 'getsourcecode',
+        address: address,
+        apikey: apiKey,
+      },
+    });
+
+    // Handle errors specific to the Etherscan API
+    if (response.data.status === '0') {
+      const result = response.data.result;
+
+      if (typeof result === 'string' && result.includes('Invalid API Key')) {
+        console.error('Etherscan API Key is invalid');
+        return res.status(500).json({ error: 'Internal Server Error: Invalid API Key' });
+      }
+
+      return res.status(404).json({ error: 'Smart contract not found' });
+    }
+
+    // Send the fetched data as response
+    return res.json(response.data);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-  
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
@@ -41,7 +81,7 @@ const PORT = process.env.PORT || 2001;
   //   const response = await axios("https://api.coinmarketcap.com/v1/ticker/?limit=0");
   //   console.log(response.data);
   // }
-  
+
   // func();
-  
+
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/components/TokenList.jsx
+++ src/components/TokenList.jsx
@@ -2,34 +2,43 @@ import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTokenListRequest } from "../redux/actions/tokenAction";
 import TokenListLogo from "../assets/img/Tokenbar-Logo.png";
-import TopToken from "./common/TopToken";
 import TokenTable from "./TokenTable";
 import TopTokenList from "./common/TopTokenList";
 import "./style.css";
-import { createTheme, useMediaQuery } from "@mui/material";
+import { createTheme, useMediaQuery, Modal, Box } from "@mui/material";
+
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const TokenList = () => {
   const theme = createTheme({
-    // Define the theme within the component
     breakpoints: {
       values: {
         xs: 0,
         sm: 600,
         md: 960,
-        lg: 1160, // Change the value of lg breakpoint here
+        lg: 1160,
         xl: 1560,
       },
     },
   });
 
-  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg")); // Use the theme with useMediaQuery
-  const MediumScreen = useMediaQuery(theme.breakpoints.down("lg")); // Use the theme with useMediaQuery
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+  const MediumScreen = useMediaQuery(theme.breakpoints.down("lg"));
 
   const [isMediumScreen, setIsMediumScreen] = useState(MediumScreen);
 
   const dispatch = useDispatch();
   useEffect(() => {
-    // Dispatch the action to fetch token data
     dispatch(fetchTokenListRequest());
   }, [fetchTokenListRequest]);
 
@@ -40,12 +49,11 @@ const TokenList = () => {
   const [filteredTokenList, setFilteredTokenList] = useState([...tokenList]);
 
   const sortedTokenList = tokenList.sort(
-    (a, b) =>
-      (b.volume24HrsETH * 1) / (b.tradeVolumeETH * 1) -
-      (a.volume24HrsETH * 1) / (a.tradeVolumeETH * 1)
+      (a, b) =>
+          (b.volume24HrsETH * 1) / (b.tradeVolumeETH * 1) -
+          (a.volume24HrsETH * 1) / (a.tradeVolumeETH * 1)
   );
 
-  // Slice the sorted token list to display only the first 7 items
   const limitedTokenList = sortedTokenList.slice(0, 10).map((item, index) => {
     return {
       num: "#" + (index + 1),
@@ -54,14 +62,18 @@ const TokenList = () => {
       symbol: item.symbol,
       logo: item.logo,
       riserate: (
-        ((item.volume24HrsETH * 1) / (item.tradeVolumeETH * 1)) *
-        100
+          ((item.volume24HrsETH * 1) / (item.tradeVolumeETH * 1)) *
+          100
       ).toFixed(2),
     };
   });
 
   const [isEditing, setIsEditing] = useState(false);
   const [text, setText] = useState("Select Token/Contract Address ⌄");
+
+  const [contractAddress, setContractAddress] = useState("");
+  const [apiResponse, setApiResponse] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     setFilteredTokenList([...tokenList]);
@@ -73,13 +85,12 @@ const TokenList = () => {
 
   const handleInputChange = (e) => {
     setFilteredTokenList(
-      [...tokenList].filter((obj) => {
-        // Check if any of the object's properties include the text
-        return (
-          obj.symbol.toLowerCase().includes(e.target.value.toLowerCase()) ||
-          obj.id.toLowerCase().includes(e.target.value.toLowerCase())
-        );
-      })
+        [...tokenList].filter((obj) => {
+          return (
+              obj.symbol.toLowerCase().includes(e.target.value.toLowerCase()) ||
+              obj.id.toLowerCase().includes(e.target.value.toLowerCase())
+          );
+        })
     );
   };
 
@@ -87,7 +98,6 @@ const TokenList = () => {
 
   const handleClickOutside = (event) => {
     if (divRef.current && !divRef.current.contains(event.target)) {
-      // Clicked outside the div
       handleSaveClick();
     }
   };
@@ -96,7 +106,6 @@ const TokenList = () => {
     setIsEditing(false);
     setFilteredTokenList([...tokenList]);
     setText("Select Token/Contract Address ⌄");
-    // Perform any save operation with the edited text here
   };
 
   useEffect(() => {
@@ -106,60 +115,106 @@ const TokenList = () => {
     };
   }, []);
 
+  // Function to fetch smart contract data
+  const fetchContractData = async (contract) => {
+    try {
+      const response = await fetch(`http://localhost:2001/api/AliFarhadiapitest/${contract}`);
+      const data = await response.json();
+      if (data.error) {
+        setApiResponse({ type: "error", message: data.error });
+      } else {
+        setApiResponse({ type: "success", message: JSON.stringify(data.result[0], null, 2) });
+      }
+      setOpenModal(true);
+    } catch (err) {
+      setApiResponse({ type: "error", message: "Failed to fetch data" });
+      setOpenModal(true);
+    }
+  };
+
+  const handleContractAddressChange = (e) => {
+    setContractAddress(e.target.value);
+  };
+
+  const handleFetchContractData = () => {
+    if (contractAddress) {
+      fetchContractData(contractAddress);
+    }
+  };
+
   return (
-    <div className="tokenlist-background font-header">
-      <img
-        src={TokenListLogo}
-        alt="Token List Logo"
-        className="token-list-logo"
-      />
-      <TopTokenList tokenList={limitedTokenList} />
-      <div
-        className="dropdown-container font-header"
-        style={{ position: "relative" }}
-      >
-        {isEditing ? (
+      <div className="tokenlist-background font-header">
+        <img src={TokenListLogo} alt="Token List Logo" className="token-list-logo"/>
+        <TopTokenList tokenList={limitedTokenList}/>
+        <div className="dropdown-container font-header" style={{position: "relative"}}>
+          {isEditing ? (
+              <input
+                  type="text"
+                  placeholder={text}
+                  className="dropdown-button"
+                  onChange={handleInputChange}
+                  style={{fontFamily: "altivo"}}
+                  autoFocus
+              />
+          ) : (
+              <button
+                  onClick={handleEditClick}
+                  className="dropdown-button"
+                  style={{overflow: "hidden", fontFamily: "altivo"}}
+              >
+                {text}
+              </button>
+          )}
+          {isEditing && (
+              <div
+                  ref={divRef}
+                  style={{
+                    position: "absolute",
+                    top: "calc(45px)",
+                    transform: "translateX(-50%)",
+                    width: isLargeScreen ? "60vw" : "100vw",
+                    backgroundColor: "#191919",
+                    padding: "10px",
+                    borderRadius: "5px",
+                    zIndex: "50",
+                    color: "white",
+                    border: "2px solid transparent",
+                    left: isLargeScreen ? "-13vw" : "-37vw",
+                  }}
+              >
+                <TokenTable tokenData={filteredTokenList}/>
+              </div>
+          )}
+        </div>
+
+        {/* Contract Address Input */}
+        <div className="contract-address-container">
           <input
-            type="text"
-            placeholder={text}
-            className="dropdown-button"
-            onChange={handleInputChange}
-            style={{ fontFamily: "altivo" }}
-            autoFocus // Automatically focus on the input field when it appears
+              type="text"
+              placeholder="Enter Smart Contract Address"
+              className="contract-input dropdown-button"
+              value={contractAddress}
+              onChange={handleContractAddressChange}
           />
-        ) : (
-          <button
-            onClick={handleEditClick}
-            className="dropdown-button"
-            style={{ overflow: "hidden", fontFamily: "altivo" }}
-          >
-            {text}
+          <button onClick={handleFetchContractData} className="dropdown-button">
+            Fetch Contract Data
           </button>
-        )}
-        {isEditing && (
-          <div
-            ref={divRef}
-            style={{
-              position: "absolute",
-              top: "calc(45px)",
-              transform: "translateX(-50%)",
-              width: isLargeScreen ? "60vw" : "100vw",
-              backgroundColor: "#191919",
-              padding: "10px",
-              borderRadius: "5px",
-              zIndex: "50",
-              color: "white",
-              border: `2px solid transparent`,
-              left: isLargeScreen ? "-13vw" : "-37vw",
-            }}
-          >
-            {/* Add your div content here */}
-            <TokenTable tokenData={filteredTokenList} />
-          </div>
-        )}
+        </div>
+
+        {/* Modal for displaying API response */}
+        <Modal
+            open={openModal}
+            onClose={() => setOpenModal(false)}
+            aria-labelledby="modal-title"
+            aria-describedby="modal-description"
+        >
+          <Box sx={modalStyle}>
+            <h2 id="modal-title">{apiResponse?.type === "error" ? "Error" : "Contract Data"}</h2>
+            <pre id="modal-description">{apiResponse?.message}</pre>
+          </Box>
+        </Modal>
       </div>
-    </div>
   );
 };
 
-export { TokenList };
+export {TokenList};
